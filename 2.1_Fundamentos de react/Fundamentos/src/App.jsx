478b68d118a49cf1a3c8c78b9fc11220762605ec
João <joaogui.silvae@gmail.com>
2025-12-19 12:11:29 -0300

    Fundamentos do react

--- 2.1_Fundamentos de react/Fundamentos/src/App.jsx
+++ 2.1_Fundamentos de react/Fundamentos/src/App.jsx
@@ -2,20 +2,26 @@ import "./App.css";
 
 // 2 - exportando primeiro componente
 import FirstComponent from "./components/FirstComponent";
-import MyComponet from "./components/MyComponet";
 
 // 4  - Template expression
 import TemplateExpression from "./components/TemplateExpression";
 
+// 5 - hierarquia de componentes
+import MyComponet from "./components/MyComponet";
+
+// 6 - Eventos
+import Events from "./components/Events";
+
 function App() {
   // 3 - comentarios (fora do jsx/html)
   return (
-    <div className="App">
+    <div>
       {/*3 - comentarios (dentro do jsx/html) */}
       <h1>fundamentos do React</h1>
       <FirstComponent />
       <TemplateExpression />
-      <MyComponet/>
+      <MyComponet />
+      <Events />
     </div>
   );
 }
--- 2.1_Fundamentos de react/Fundamentos/src/components/FirstComponent.jsx
+++ 2.1_Fundamentos de react/Fundamentos/src/components/FirstComponent.jsx
@@ -1,8 +1,11 @@
+import MyComponet from "./MyComponet";
+
 // 1 - Criando primeiro componente
 const FirstComponent = () => {
   return (
     <div>
       <h2>Meu primeiro componente</h2>
+      <MyComponet />
     </div>
   );
 };
